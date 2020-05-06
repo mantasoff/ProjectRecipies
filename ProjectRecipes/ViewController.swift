b5c0ba0d167c7a7371eda6f5343915d631a15138
mantasoff <mantas.paskevicius1996@gmail.com>
2020-05-06 20:38:41 +0300

    Initial Commit

---
+++ ProjectRecipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectRecipes
+//
+//  Created by Mantas Paškevičius on 2020-05-06.
+//  Copyright © 2020 Mantas Paškevičius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProjectRecipesTests/ProjectRecipesTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProjectRecipesTests.swift
+//  ProjectRecipesTests
+//
+//  Created by Mantas Paškevičius on 2020-05-06.
+//  Copyright © 2020 Mantas Paškevičius. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectRecipes
+
+class ProjectRecipesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
